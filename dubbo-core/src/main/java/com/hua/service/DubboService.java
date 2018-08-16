d65a0d778eefb9030af6387b5f97798999a51535
qianye.zheng <dearzqy@163.com>
2018-08-16 15:49:57 +0800

    no message

---
+++ dubbo-core/src/main/java/com/hua/service/DubboService.java
@@ -0,0 +1,16 @@
+/**
+ * DubboService.java
+ * @author  qye.zheng
+ * 	version 1.0
+ */
+package com.hua.service;
+
+/**
+ * DubboService
+ * 描述: Apache Dubbo - 服务
+ * @author  qye.zheng
+ */
+public interface DubboService extends CoreService
+{
+
+}
---
+++ dubbo-template/src/main/java/com/hua/service/impl/DubboServiceImpl.java
@@ -0,0 +1,19 @@
+/**
+ * DubboServiceImpl.java
+ * @author  qye.zheng
+ * 	version 1.0
+ */
+package com.hua.service.impl;
+
+import com.hua.service.DubboService;
+
+/**
+ * DubboServiceImpl
+ * 描述: Apache Dubbo - 服务
+ * @author  qye.zheng
+ */
+public final class DubboServiceImpl extends CoreServiceImpl implements
+		DubboService
+{
+
+}
